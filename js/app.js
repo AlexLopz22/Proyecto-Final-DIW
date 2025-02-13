2fbdc6840bbc3c9ed4b36268416fe10ebd93b735
Alejandro López Olmedo <alex05brx@gmail.com>
2025-02-13 15:58:05 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menu = document.getElementById("menu-enlaces2");
-    const botonMenu = document.getElementById("boton-menu"); 
-    const sidebar = document.getElementById("sidebar-social");
-    const botonSidebar = document.getElementById("boton-sidebar");
-
-    botonSidebar.addEventListener("click", function(){
-        if(sidebar.classList.contains("sidebar-activa")){
-            sidebar.classList.remove("sidebar-activa");
-        } else{
-            sidebar.classList.add("sidebar-activa");
-        }
-    })
+    const botonMenu = document.getElementById("boton-menu");
 
     botonMenu.addEventListener("click", function() {
         if (menu.classList.contains("mostrar")) {
