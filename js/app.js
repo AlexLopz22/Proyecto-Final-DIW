ae9fcf89d7bcd7d94700c79911be3c714d9ed1e1
AlexLopz22 <alex05brx@gmail.com>
2025-03-04 11:58:11 +0100

    2º Update v1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menu = document.getElementById("menu-enlaces2");
     const botonMenu = document.getElementById("boton-menu");
+    const botonesApuestas = document.getElementById("botones-apuestas");
+    const finalPagina = document.getElementById("final-pagina");
+
+    // Observar si el final de la página es visible
+    const observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+            botonesApuestas.style.position = 'absolute';
+            botonesApuestas.style.right = '10px';
+        } else {
+            botonesApuestas.style.position = 'fixed';
+            botonesApuestas.style.right = '40px';
+        }
+    });
+
+    observer.observe(finalPagina);
 
     botonMenu.addEventListener("click", function() {
         if (menu.classList.contains("mostrar")) {
