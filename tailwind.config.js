289912ebe97a40a931af16a660631b0513091e7a
AlexLopz22 <alex05brx@gmail.com>
2025-03-10 13:05:22 +0100

    Fixing errors

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./pages/apuestas.html",  
+      "./src/**/*.css", 
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
